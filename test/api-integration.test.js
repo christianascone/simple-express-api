6f529bf7b220384032485a6909ef4f8a7c519dca
ChristianAscone <ascone.christian@gmail.com>
2017-10-26 10:33:27 +0200

    Add test dependencies and test functions

---
+++ test/api-integration.test.js
@@ -0,0 +1,66 @@
+var supertest = require("supertest");
+var should = require("chai").should();
+
+var server = supertest.agent("http://localhost:3000");
+
+describe("Test GET /users", function() {
+
+  it("should return a list of 10 users (default size)", function(done) {
+    server.get("/api/users")
+    .expect("Content-type", /json/)
+    .expect(200)
+    .end(function(err, res) {
+      res.status.should.equal(200);
+      res.body.success.should.equal(1);
+      res.body.data.length.should.equal(10);
+      done();
+    });
+  });
+
+  it("should return a list of 5 users (passed parameter)", function(done) {
+    server.get("/api/users")
+    .query({
+      size: 5
+    })
+    .expect("Content-type", /json/)
+    .expect(200)
+    .end(function(err, res) {
+      res.status.should.equal(200);
+      res.body.success.should.equal(1);
+      res.body.data.length.should.equal(5);
+      done();
+    });
+  });
+});
+
+describe("Test POST /users", function() {
+
+  it("should return a user with same data passed as parameters", function(done) {
+    server.post("/api/user")
+    .send({
+      first_name: "Name",
+      last_name: "Not Name"
+    })
+    .expect("Content-type", /json/)
+    .expect(200)
+    .end(function(err, res) {
+      res.status.should.equal(200);
+      res.body.success.should.equal(1);
+      res.body.data.first_name.should.equal("Name");
+      res.body.data.last_name.should.equal("Not Name");
+      done();
+    });
+  });
+
+  it("should return an empty user", function(done) {
+    server.post("/api/user")
+    .expect("Content-type", /json/)
+    .expect(200)
+    .end(function(err, res) {
+      res.status.should.equal(200);
+      res.body.success.should.equal(1);
+      res.body.data.should.be.empty;
+      done();
+    });
+  });
+});
